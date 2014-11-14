f5eb86c34ce9b30910c18507fe8984ccbbd7fd6a
pegnovi <pegnovi@yahoo.com>
2014-11-13 17:57:46 -0800

    Added in the handshake between clients
    Just added the WebRTC handshake between the clients so the connection is
    established.
    No sending of data yet using dataChannels not so sure how and when to
    set it up in code.

--- public/client.js
+++ public/client.js
@@ -1,5 +1,67 @@
 $(document).ready(function() {
 
+	//============
+	//===WEBRTC===
+	//============
+	//http://www.html5rocks.com/en/tutorials/webrtc/infrastructure/
+	//http://www.html5rocks.com/en/tutorials/webrtc/basics/
+	//https://www.webrtc-experiment.com/docs/how-to-use-rtcdatachannel.html#sctp-firefox
+	//https://bitbucket.org/webrtc/codelab
+	
+	var PeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+	var SessionDescription = window.RTCSessionDescription || window.mozRTCSessionDescription || window.webkitRTCSessionDescription;
+	/*
+	var GET_USER_MEDIA = navigator.getUserMedia ? "getUserMedia" :
+                     navigator.mozGetUserMedia ? "mozGetUserMedia" :
+                     navigator.webkitGetUserMedia ? "webkitGetUserMedia" : "getUserMedia";
+	//var v = document.createElement("video");
+	/*var SRC_OBJECT = 'srcObject' in v ? "srcObject" :
+					'mozSrcObject' in v ? "mozSrcObject" :
+					'webkitSrcObject' in v ? "webkitSrcObject" : "srcObject";
+	*/
+	/*
+	// The RTCIceCandidate object.
+	var RTCIceCandidate = mozRTCIceCandidate;
+	console.log(PeerConnection);
+	console.log(SessionDescription);
+	console.log(RTCIceCandidate);
+	*/
+	
+	var pc = new PeerConnection();
+	// Establish your peer connection using your signaling channel here
+	var dataChannel = pc.createDataChannel("dataChannel");
+	dataChannel.onmessage = function(event) {
+		console.log("received: " + event.data);
+	};
+	dataChannel.onopen = function() {
+		console.log("datachannel open");
+	}
+	dataChannel.onclose = function() {
+		console.log("datachannel close");
+	}
+
+	/*
+	peerConnection.createOffer(
+		function(sessionDesc) {
+			peerConnection.setLocalDescription(sessionDesc);
+			console.log("Sending: SDP");
+			console.log(sessionDesc);
+			//socket.emit("???", sessionDesc);
+		},
+		function onCreateSessionDescriptionError(error) {
+			console.log('Failed to create session description: ' + error.toString());
+		}
+	);
+	*/
+	
+	console.log(pc);
+	console.log(dataChannel);
+	
+	console.log("WEBRTC DONE FOR NOW");
+	//============
+	//============
+	//============
+
 	console.log("connecting...");
 	var socket = io.connect("127.0.0.1:8080");
 	console.log("connected!!!");
@@ -13,9 +75,17 @@ $(document).ready(function() {
 	var roomId = getRoomIdFromUrl();
 	if(roomId) {
 		showLoading();
-		socket.emit("doesroomexist", JSON.stringify({
-			room: roomId
-		}));
+		
+		pc.createOffer(function(offer) {
+			pc.setLocalDescription(new SessionDescription(offer), function() {
+				//send the offer to a server to be forwarded to the friend you're calling
+				socket.emit("doesroomexist", JSON.stringify({
+					room: roomId,
+					clientOffer: offer
+				}));
+			}, error);
+		}, error);
+		
 	} else {
 		showHome();
 		socket.emit("createid");
@@ -35,6 +105,7 @@ $(document).ready(function() {
 		}
 	});
 
+	
 	socket.on("doesroomexist", function(data) {
 		data = JSON.parse(data);
 		if(data.result) {
@@ -45,7 +116,34 @@ $(document).ready(function() {
 			showHome();
 		}
 	});
+	
+	socket.on("offerFromClient", function(data) {
+		data = JSON.parse(data);
+		console.log(data.offer);
+		console.log("OFFER RECEIVED!!!");
+		
+		pc.setRemoteDescription(new SessionDescription(data.offer), function() {
+			pc.createAnswer(function(answer) {
+				pc.setLocalDescription(new SessionDescription(answer), function() {
+					//send the answer to a server to be forwarded back to the caller
+					socket.emit("sendAnswer", JSON.stringify({
+						hostClientAnswer: answer,
+						targetClient: data.theClientID 
+					}));
+				}, error);
+			}, error);
+		}, error);
+		
+	});
 
+	socket.on("hostAnswer", function(data) {
+		data = JSON.parse(data);
+		console.log("ANSWER RECEIVED!!!");
+		
+		pc.setRemoteDescription(new SessionDescription(data.hostAnswer), function() {}, error);
+		
+	});
+	
 	$("#create-button").click(function() {
 		var string = "?i=".concat($("#input-room-id").val());
 		history.replaceState(null, "", string);
@@ -109,3 +207,5 @@ function hide() {
 	$("#navbar").hide();
 	$("#loading").hide();
 }
+
+function error(err) { console.log("ERROR OCCURRED!!!"); endCall(); }
